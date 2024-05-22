207ccdf6a1ba9f177106d0bf646ccf4ab0f8b3fa
yunshin <2younsin@gmail.com>
2024-05-22 17:26:22 +0900

    feat : Navigation 컴포넌트, Layout 에 포함

--- studies/next-js/nomad-coder/practice/app/page.tsx
+++ studies/next-js/nomad-coder/practice/app/page.tsx
@@ -1,12 +1,8 @@
-import Navigation from "../components/navigation"
-
 const Page = () => {
   return (
     <div>
-      <Navigation />
       <h1>Hello</h1>
     </div>
   )
 }
-
 export default Page
